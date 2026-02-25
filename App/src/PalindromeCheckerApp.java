0326ac191daa9930b8d602cace83d49545c0bcbf
Drishti <yadav2006drishti@gmail.com>
2026-02-25 11:17:07 +0530

    Drishti Added uc9

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -230,3 +230,43 @@ public class UseCase8PalindromeCheckerApp {
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
+public class UseCase9PalindromeCheckerApp {
+
+    /**
+     * Application entry point for UC9.
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        boolean result = check(input, 0, input.length() - 1);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+    }
+
+    /**
+     * Recursively checks whether a string is palindrome.
+     *
+     * @param s Input string
+     * @param start Starting index
+     * @param end Ending index
+     * @return true if palindrome, otherwise false
+     */
+    private static boolean check(String s, int start, int end) {
+
+        // Base condition: If pointers cross, it's a palindrome
+        if (start >= end) {
+            return true;
+        }
+
+        // If mismatch found
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call for inner substring
+        return check(s, start + 1, end - 1);
+    }
+}
